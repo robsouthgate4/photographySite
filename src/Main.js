b16ce17844b459d1d27fea403ef269d84712f8b5
Rob Southgate <rob.southgate@telegraph.co.uk>
2017-03-21 16:03:07 +0000

    Trying to make router work with redux props

--- src/Main.js
+++ src/Main.js
@@ -1,25 +1,11 @@
 import React, { PropTypes } from 'react'
-import { connect } from 'react-redux'
-import store from './store';
 
-const Main = React.createClass({
-  render () {
-    return (
-      <div className="main">
-        {React.cloneElement(this.props.children, {...this.props})}
-      </div>
-    )
-  }
-});
+const Main = (props) => {
+  return (
+    <div className="main">
+      {props.children}
+    </div>
+  )
+}
 
 export default Main;
-
-// const Main = (props) => {
-//   return (
-//     <div className="main">
-//       {React.cloneElement(props.children, {...props})}
-//     </div>
-//   )
-// }
-//
-// export default Main;
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Router, Route, browserHistory } from 'react-router';
 import Main from './Main';
 import App from './App';
-import { Dashboard, Login, Projects } from './components/pages';
+import { Dashboard, Login } from './components/pages';
+import { ProjectList } from './components/projects';
 import { isUserAuthenticated, deauthenticateUser } from './lib/Auth';
 
 const logoutUser = (nextState, replace, callBack) => {
@@ -23,8 +24,8 @@ const Routes = (props) => (
       <Route path="/" component={App}>
         <Route path="login" component={Login} onEnter={(nextState, replace, callBack) => loginUser(nextState, replace, callBack)}></Route>
         <Route path="dashboard" component={Dashboard} onEnter={(nextState, replace, callBack) => checkLoggedIn(nextState, replace, callBack)}>
-            <Route path="projects" component={Projects}></Route>
-            <Route path="instagram" component={Projects}></Route>{/*TODO create IG component*/}
+            <Route path="projects" component={ProjectList}></Route>
+            <Route path="instagram" component={ProjectList}></Route>{/*TODO create IG component*/}
             <Route path="logout" onEnter={(nextState, replace, callBack) => logoutUser(nextState, replace, callBack)}></Route>
         </Route>
       </Route>
